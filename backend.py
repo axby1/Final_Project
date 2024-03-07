24d5f48729c208c07408168bb4b3accdd287d2b4
Abby <99085097+axby1@users.noreply.github.com>
2024-03-07 15:44:55 +0530

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from optimised_algo import main  # Import your Python script or function here
+
+app = Flask(__name__)
+
+@app.route('/classify', methods=['POST'])
+def classify_url():
+    data = request.get_json()
+    url = data.get('url')
+
+    # Process the URL using your Python script or function
+
+    category = main(url)
+
+    return jsonify({'category': category})
+
+if __name__ == '__main__':
+    app.run(port=3000)
